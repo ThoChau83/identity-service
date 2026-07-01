5de68428537fe1d5b784875c1be02b8743f40410
Tho Chau <chauphuoctho2006@gmail.com>
2026-07-01 21:49:39 +0700

    create, sign, verify a JWT

--- src/main/java/org/example/identityservice/controller/AuthenticationController.java
+++ src/main/java/org/example/identityservice/controller/AuthenticationController.java
@@ -1,17 +1,22 @@
 package org.example.identityservice.controller;
 
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.example.identityservice.dto.request.AuthenticationRequest;
+import org.example.identityservice.dto.request.IntrospectRequest;
 import org.example.identityservice.dto.response.ApiResponse;
 import org.example.identityservice.dto.response.AuthenticationResponse;
+import org.example.identityservice.dto.response.IntrospectResponse;
 import org.example.identityservice.service.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,17 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
-    @PostMapping("/log-in")
+    @PostMapping("/token")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
-      boolean result = authenticationService.authenticate(request);
+      var result = authenticationService.authenticate(request);
 
-        return ApiResponse.<AuthenticationResponse>builder() // Lấy cái hộp TO ra
-                .result( // Mở cái khe "result" của hộp to ra, bắt đầu nhét đồ vào:
-                        AuthenticationResponse.builder() // Lấy cái hộp NHỎ ra
-                                .authenticated(result)    // Bỏ kết quả vào hộp nhỏ
-                                .build()                  // Đóng nắp hộp NHỎ
-                ) // Hết quá trình nhét đồ vào hộp to
-                .build(); // Đóng nắp hộp TO lại, và gửi về (return)
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
+                .build();
     }
 
 //    Cach thu cong
---
+++ src/main/java/org/example/identityservice/dto/request/IntrospectRequest.java
@@ -0,0 +1,13 @@
+package org.example.identityservice.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Data
+@NoArgsConstructor       // <--- THÊM DÒNG NÀY (Để Jackson đọc được JSON)
+@AllArgsConstructor      // <--- THÊM DÒNG NÀY (Để Builder hoạt động)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectRequest {
+    String token;
+}
